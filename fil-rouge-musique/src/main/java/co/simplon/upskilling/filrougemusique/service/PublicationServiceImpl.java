0059bdee75efb19d081532ee02e1bc8af5865389
FranckLob <franck.lobjois.devweb@gmail.com>
2020-03-03 17:03:44 +0100

    First commit Franck

--- fil-rouge-musique/src/main/java/co/simplon/upskilling/filrougemusique/service/PublicationServiceImpl.java
+++ fil-rouge-musique/src/main/java/co/simplon/upskilling/filrougemusique/service/PublicationServiceImpl.java
@@ -3,6 +3,7 @@ package co.simplon.upskilling.filrougemusique.service;
 import co.simplon.upskilling.filrougemusique.model.*;
 import co.simplon.upskilling.filrougemusique.repository.PublicationRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -18,37 +19,45 @@ public class PublicationServiceImpl implements PublicationService {
     }
 
     @Override
-    public Page<Publication> getPublications(Pageable pageable) {
-        return publicationRepository.findAll(pageable);
+    public Page<Publication> getPublications(Integer pageNumber, Integer pageSize) {
+        return publicationRepository.findAll(PageRequest.of(returnPageNumber(pageNumber), returnPageSize(pageSize, 10)));
     }
 
     @Override
-    public Page<Publication> getFilteredPublicationsByAppUser(Long appUserId, Pageable pageable) {
-        return publicationRepository.getPublicationsByAppUser_IdEquals(appUserId, pageable);
+    public Page<Publication> getFilteredPublicationsByAppUser(Long appUserId, Integer pageNumber, Integer pageSize) {
+        return publicationRepository.getPublicationsByAppUser_IdEquals(appUserId, PageRequest.of(returnPageNumber(pageNumber), returnPageSize(pageSize, 10)));
     }
 
     @Override
-    public List<Publication> getFilteredPublicationsByArtist(Artist artist) {
-        return null;
+    public Page<Publication> getFilteredPublicationsByArtist(Long artistId, Integer pageNumber, Integer pageSize) {
+        return publicationRepository.getPublicationsByArtist_IdEquals(artistId, PageRequest.of(returnPageNumber(pageNumber), returnPageSize(pageSize, 10)));
     }
 
     @Override
-    public List<Publication> getFilteredPublicationsByArtwork(Artwork artwork) {
-        return null;
+    public Page<Publication> getFilteredPublicationsByArtwork(Long artworkId, Integer pageNumber, Integer pageSize) {
+        return publicationRepository.getPublicationsByArtwork_IdEquals(artworkId, PageRequest.of(returnPageNumber(pageNumber), returnPageSize(pageSize, 10)));
     }
 
     @Override
-    public List<Publication> getFilteredPublicationsByTitle(Title title) {
-        return null;
+    public Page<Publication> getFilteredPublicationsByTitle(Long titleId, Integer pageNumber, Integer pageSize) {
+        return publicationRepository.getPublicationsByTitle_IdEquals(titleId, PageRequest.of(returnPageNumber(pageNumber), returnPageSize(pageSize, 10)));
     }
 
-    @Override
-    public List<Publication> getFilteredPublicationsByStyle(Style style) {
-        return null;
+//    @Override
+//    public List<Publication> getFilteredPublicationsByStyle(Style style) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Publication> getFilteredPublicationsByType(Type type) {
+//        return null;
+//    }
+
+    public int returnPageNumber(Integer pageNumber) {
+        return (pageNumber != null) ? pageNumber : 0;
     }
 
-    @Override
-    public List<Publication> getFilteredPublicationsByType(Type type) {
-        return null;
+    public int returnPageSize(Integer pageSize, int size) {
+        return (pageSize != null) ? pageSize : size;
     }
 }
