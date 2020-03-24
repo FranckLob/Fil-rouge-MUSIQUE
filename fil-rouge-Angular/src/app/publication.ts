00dfeb3ce32792f99ace643b46659744d3db9124
Yves Benoit <yves.benoit.devweb@gmail.com>
2020-03-24 16:39:59 +0100

    une liste de publications s affiche

---
+++ fil-rouge-Angular/src/app/publication.ts
@@ -0,0 +1,14 @@
+
+export interface Publication {
+    id:number;
+    appUser:string;
+    artist:string;
+    date : Date;
+    artwork:string;
+    title:string;
+    style:string;
+    type:string;
+    author:string;
+    composer:string;
+    snippet:string;
+}
